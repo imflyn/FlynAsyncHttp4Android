d097c6c842a22edee38739dd956f12f6a1190dc2
imflyn <imflyn@163.com>
2014-07-13 21:37:57 +0800

    添加同步方法

---
+++ ActiveVolcano/src/com/flyn/volcano/RequestFuture.java
@@ -0,0 +1,135 @@
+package com.flyn.volcano;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class RequestFuture<T> implements Future<T>
+{
+    private final Listener mListner;
+    private Request<?>     mRequest;
+    private boolean        mResultReceived = false;
+    private T              mResult;
+    private Throwable      mException;
+
+    public static <E> RequestFuture<E> newFuture()
+    {
+        return new RequestFuture<E>();
+    }
+
+    private RequestFuture()
+    {
+        mListner = new Listener()
+        {
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onSuccess(Response<?> response)
+            {
+                mResultReceived = true;
+                mResult = (T) response.getResult();
+                notifyAll();
+            }
+
+            @Override
+            public void onFailure(Throwable error)
+            {
+                mException = error;
+                notifyAll();
+            }
+        };
+    }
+
+    public void setRequest(Request<?> request)
+    {
+        mRequest = request;
+        mRequest.setListener(mListner);
+    }
+
+    @Override
+    public synchronized boolean cancel(boolean mayInterruptIfRunning)
+    {
+        if (mRequest == null)
+        {
+            return false;
+        }
+
+        if (!isDone())
+        {
+            mRequest.cancel();
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public T get() throws InterruptedException, ExecutionException
+    {
+        try
+        {
+            return doGet(null);
+        } catch (TimeoutException e)
+        {
+            throw new AssertionError(e);
+        }
+    }
+
+    @Override
+    public T get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException
+    {
+        return doGet(TimeUnit.MILLISECONDS.convert(timeout, unit));
+    }
+
+    private synchronized T doGet(Long timeoutMs) throws InterruptedException, ExecutionException, TimeoutException
+    {
+        if (mException != null)
+        {
+            throw new ExecutionException(mException);
+        }
+
+        if (mResultReceived)
+        {
+            return mResult;
+        }
+
+        if (timeoutMs == null)
+        {
+            wait(0);
+        } else if (timeoutMs > 0)
+        {
+            wait(timeoutMs);
+        }
+
+        if (mException != null)
+        {
+            throw new ExecutionException(mException);
+        }
+
+        if (!mResultReceived)
+        {
+            throw new TimeoutException();
+        }
+
+        return mResult;
+    }
+
+    @Override
+    public boolean isCancelled()
+    {
+        if (mRequest == null)
+        {
+            return false;
+        }
+        return mRequest.isCanceled();
+    }
+
+    @Override
+    public synchronized boolean isDone()
+    {
+        return mResultReceived || mException != null || isCancelled();
+    }
+
+}
